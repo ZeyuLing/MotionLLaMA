fa47c484f9ff3b2645a681d8eab8fa5a52e9102c
zyling <56373914+ZeyuLing@users.noreply.github.com>
2024-11-26 21:00:47 +0800

    Delete configs directory from project page branch

--- configs/wav_tokenizer/wav_tokenizer_large_unify_600_24k_4096.py
+++
@@ -1,4 +0,0 @@
-model = dict(
-    type='WavTokenizer',
-    pretrained='checkpoints/wav_tokenizer/wav_tokenizer_large_unify_600_24k_4096',
-)
--- configs/wav_tokenizer/wav_tokenizer_small_320_24k_4096.py
+++
@@ -1,4 +0,0 @@
-model = dict(
-    type='WavTokenizer',
-    pretrained='checkpoints/wav_tokenizer/wav_tokenizer_small_320_24k_4096',
-)
